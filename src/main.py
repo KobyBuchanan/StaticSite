2cdb4a7f1ec2e9f3bfefd1528cd14eb54c817113
KobyBuchanan <kobya.buchanan@gmail.com>
2024-06-28 09:54:55 -0400

    created all files from Ch.1

---
+++ src/main.py
@@ -0,0 +1,10 @@
+from textnode import TextNode
+def main():
+    text = "This is a test node"
+    text_type = "bold"
+    url = 'https://www.boot.dev'
+    test_node = TextNode(text, text_type,url)
+    print(test_node)
+
+main()
+# hello world
